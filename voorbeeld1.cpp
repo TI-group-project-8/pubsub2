23ddc43eab2865aea16136534d7367886a4ec2fd
nick teeuwen <nickteeuwen21@gmail.com>
2021-04-01 13:36:36 +0200

    fix test 3

--- voorbeeld1.cpp
+++ voorbeeld1.cpp
@@ -7,32 +7,33 @@
 #include <vector>
 
 
-int main(){
+int main() {
 
-	//maak een publisher voor een topic
-	//parameter 1: naam van de topic (mag geen spaties bevatten)
-	publisher p("mytopic");
+    //maak een publisher voor een topic
+    //parameter 1: naam van de topic (mag geen spaties bevatten)
+    publisher p("mytopic");
 
-	vector<string> the_code = {};
-	string s;
-	cout << "voer berichten (zonder spaties) in om te verzenden" << endl;
-	cout << "\"stop\" om te stoppen" << endl;
-	while(true) {
+    vector<string> the_code = {};
+    string s;
+    cout << "voer berichten (zonder spaties) in om te verzenden" << endl;
+    cout << "\"stop\" om te stoppen" << endl;
+    while (true) {
         cout << "? ";
         cin >> s;
-    
-        if (s == "stop"){
+
+        if (s == "stop") {
             break;
-        //verzend een bericht naar de subscribers van dit topic
-        }else{
-	        the_code.push_back(s);
-	        if(the_code.size() > 4){
-	            for(int i = 0; i < the_code.size(); i++){
-	                p.send(the_code[i]);
-	            }
-	            p.send("----------------");
-	            the_code = {};
-	        }
-	}
-	return 0;
+            //verzend een bericht naar de subscribers van dit topic
+        } else {
+            the_code.push_back(s);
+            if (the_code.size() > 4) {
+                for (int i = 0; i < the_code.size(); i++) {
+                    p.send(the_code[i]);
+                }
+                p.send("----------------");
+                the_code = {};
+            }
+        }
+        return 0;
+    }
 };
